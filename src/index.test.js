35a4307caa157d3a94923ca2c017ce584d37ddb6
Ossaija ThankGod <codebugsolved@gmail.com>
2017-07-15 23:19:58 +0100

    test(build): Add travis config, conventional commit, and semantic-release and added test
    #1

---
+++ src/index.test.js
@@ -0,0 +1,26 @@
+'use strict'
+const expect = require('chai').expect
+const starWarsNames = require('./index')
+
+describe('starwars-names', () => {
+  describe('all', () => {
+    it('should be an array of strings', () => {
+      expect(starWarsNames.all).to.satisfies(isArrayOfStrings)
+      function isArrayOfStrings(array) {
+        return array.every(item => typeof item === 'string')
+      }
+    })
+
+    it('should contain Luke Skywalker', () => {
+      expect(starWarsNames.all).to.include('Luke Skywalker')
+    })
+  })
+
+  describe('random', () => {
+    it('it should return a random item from starwarsNames.all', () => {
+      const randomItem = starWarsNames.random()
+      expect(starWarsNames.all).to.include(randomItem)
+    })
+  })
+})
+
